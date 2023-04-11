360a5251b0e132716f7614acd8880f882a770779
amal <amalashraf04@gmail.com>
2023-04-11 11:27:41 +0530

    new commit

---
+++ models/book.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const BookSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    about: {
+        type: String,
+        required: true
+    },
+    language:{ type: String,
+    required: true
+    }
+})
+
+
+let BookDATA = mongoose.model('bookdetail', BookSchema)
+
+module.exports = BookDATA
